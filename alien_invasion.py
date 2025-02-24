4a7d166bbe7be78e82b22b45e529cb00114852f8
Vitalii <nn778n9@gmail.com>
2025-02-24 18:07:03 +0200

    End of 12 chapter

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Загальний клас програми що керує ресурсами та поведінкою гри"""
@@ -14,20 +15,31 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_wight, self.settings.screen_height))
-        pygame.display.set_caption("Alien Invasion")
+        """
+        #Повноекранний режим гри
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        """
 
-        self.ship = Ship(self)
+        #Віконний режим гри
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
 
         #Задати колір фону
         self.bg_image = pygame.image.load("images/space.bmp")
+
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Запуск головного циклу гри"""
 
         while True:
             self._check_events()
+            self.ship.update()
+            self._bullets_update()
             self._update_screen()
 
     def _check_events(self):
@@ -35,12 +47,52 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_event(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_event(event)
+
+    def _check_keydown_event(self, event):
+        """Відслідковувати натиснуті клавіші"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_event(self, event):
+        """Відслідковувати відтиснуті клавіші"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         #Наново перемалювати екран
         #self.screen.fill(self.settings.bg_color)
         self.screen.blit(self.bg_image, (0,0))
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+    def _bullets_update(self):
+        """Оновлення пуль як були випущені та зниклі"""
+        #Оновлення позиції куль
+        self.bullets.update()
+
+        #Видалення пуль які зникли
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
         #Показати останній намальований екран
         pygame.display.flip()
--- settings.py
+++ settings.py
@@ -5,6 +5,16 @@ class Settings:
         """Ініціалізувати налаштування гри"""
 
         #Налаштування екрану
-        self.screen_wight = 1200
+        self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        #Налаштування корабля
+        self.ship_speed = 1.5
+        
+        #Налаштування кулі
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_heigth = 15
+        self.bullet_color = (175, 238, 238)
+        self.bullets_allowed = 3
